f83007667639983a7ba2c77b0713165a6afa0a79
Petr Filip <petafilip@outlook.cz>
2022-01-04 15:11:07 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LogikHra
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnEvaluate = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(331, 368);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnEvaluate
+            // 
+            this.btnEvaluate.Location = new System.Drawing.Point(12, 386);
+            this.btnEvaluate.Name = "btnEvaluate";
+            this.btnEvaluate.Size = new System.Drawing.Size(155, 51);
+            this.btnEvaluate.TabIndex = 1;
+            this.btnEvaluate.Text = "Vyhodnotit";
+            this.btnEvaluate.UseVisualStyleBackColor = true;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(185, 386);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(158, 51);
+            this.btnNewGame.TabIndex = 2;
+            this.btnNewGame.Text = "Nová hra";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 444);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnEvaluate);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Logik";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnEvaluate;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
---
+++ Pin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace LogikHra
+{
+    class Pin
+    {
+        public PinState State { get; set; }
+
+        public Pin()
+        {
+            State = PinState.Empty;
+        }
+        //vykreslování
+        public void Draw(Graphics graphics, PointF location, int size)
+        {
+            Brush brush = new SolidBrush(GetColor());
+            graphics.FillEllipse(Brushes.Yellow, location.X, location.Y, size, size);
+            graphics.DrawEllipse(Pens.Black, location.X, location.Y, size, size);
+
+        }
+
+        private Color GetColor()
+        {
+            switch (State)
+            {
+                case PinState.Color1:
+                    return Color.Red;
+                case PinState.Color2:
+                    return Color.Green;
+                case PinState.Color3:
+                    return Color.Blue;
+                case PinState.Color4:
+                    return Color.Cyan;
+                case PinState.Color5:
+                    return Color.Magenta;
+                case PinState.Color6:
+                    return Color.Yellow;
+                default:
+                    return Color.White;
+            }
+        }
+
+        public enum PinState
+        {
+            Empty,
+            Color1,
+            Color2,
+            Color3,
+            Color4,
+            Color5,
+            Color6
+        }
+    }
+}
